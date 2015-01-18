79d14338161c4a0eba5a2954216db9beca3da55b
Gavin <wingman639@yahoo.com.cn>
2015-01-18 22:25:20 +0800

    GavinBot radar lock target

--- src/gavin/GavinBot.java
+++ src/gavin/GavinBot.java
@@ -8,8 +8,15 @@ import robocode.ScannedRobotEvent;
 public class GavinBot extends AdvancedRobot
 {
 	Target target = new Target();
+	
 	public class Target {
+		public ScannedRobotEvent e = null;
+		public double absoluteDirectionRadains = 0;
 		
+		public void update( ScannedRobotEvent inE, AdvancedRobot me) {
+			e = inE;
+			absoluteDirectionRadains = e.getBearingRadians() + me.getHeadingRadians();
+		}
 	}
 
     public void run() 
@@ -18,11 +25,38 @@ public class GavinBot extends AdvancedRobot
         setAdjustRadarForGunTurn( true ); 
         this.setColors(Color.blue, Color.black, Color.white, Color.blue, Color.cyan);
         
-        
+        while (true) {
+        	if (target.e == null) {
+        		setTurnRadarRight(360);
+        		execute();
+        	}
+        	else {
+        		execute();
+        	}
+        }
     } 
 
     public void onScannedRobot( ScannedRobotEvent e ) 
     {
-
+    	target.update(e, this);
+    	lockRadarOnTarget();
+    }
+    
+    public void lockRadarOnTarget() {
+    	double angerDiff = minAngleRadians(target.absoluteDirectionRadains - this.getRadarHeadingRadians());
+    	setTurnRadarRightRadians( angerDiff * 1.5 );
+    }
+    
+    public double minAngleRadians( double angle ) {
+    	if (angle > 2 * Math.PI) {
+    		angle = angle % (2 * Math.PI);
+    	}
+    	if (angle > Math.PI ) {
+    		return 2 * Math.PI - angle;
+    	}
+    	if (angle < -Math.PI) {
+    		return 2 * Math.PI + angle;
+    	}
+    	return angle;
     }
 }
