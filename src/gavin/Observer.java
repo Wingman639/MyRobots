3f7155ede2d0b93dd13fe5c6946d01c91a3b4f68
Gavin <wingman639@yahoo.com.cn>
2015-01-18 20:35:36 +0800

    rename functions

--- src/gavin/Observer.java
+++ src/gavin/Observer.java
@@ -25,14 +25,16 @@ public class Observer extends AdvancedRobot {
 	         
 	public void onScannedRobot(ScannedRobotEvent e) 
 	{ 
-	        enemy.update(e,this); 
-	    double Offset = rectify( enemy.direction - getRadarHeadingRadians() ); 
+	    enemy.update(e, this); 
+	    double Offset = miniOffset( enemy.direction - getRadarHeadingRadians() ); 
 	    setTurnRadarRightRadians( Offset * 1.5); 
 	    turnGunToRadar();
 	    fire(1);
 	}
-	//角度修正方法，重要
-	public double rectify ( double angle ) 
+	
+
+	
+	public double miniOffset ( double angle ) 
 	{ 
 	    if ( angle < -Math.PI ) 
 	        angle += 2*Math.PI; 
@@ -41,9 +43,9 @@ public class Observer extends AdvancedRobot {
 	    return angle; 
 	}
 	public class Enemy {
-	        public double x,y;
-	        public String name = null;
-	        public double headingRadian = 0.0D; 
+	    public double x,y;
+	    public String name = null;
+	    public double headingRadian = 0.0D; 
 	    public double bearingRadian = 0.0D; 
 	    public double distance = 1000D; 
 	    public double direction = 0.0D; 
@@ -56,9 +58,9 @@ public class Observer extends AdvancedRobot {
 	            name = e.getName();
 	            headingRadian = e.getHeadingRadians();
 	            bearingRadian = e.getBearingRadians();
-	            this.energy = e.getEnergy();
-	            this.velocity = e.getVelocity();
-	            this.distance = e.getDistance();
+	            energy = e.getEnergy();
+	            velocity = e.getVelocity();
+	            distance = e.getDistance();
 	            direction = bearingRadian + me.getHeadingRadians(); 
 	            x = me.getX() + Math.sin( direction ) * distance; 
 	            y= me.getY() + Math.cos( direction ) * distance; 
@@ -66,7 +68,40 @@ public class Observer extends AdvancedRobot {
 	}
 	
 	public void turnGunToRadar() {
-		double offset = rectify(getRadarHeadingRadians() - getGunHeadingRadians());
+		double offset = miniOffset(getRadarHeadingRadians() - getGunHeadingRadians());
 		setTurnGunRightRadians(offset);
+	}
+	
+	/**This function predicts the time of the intersection between the 
+	bullet and the target based on a simple iteration.  It then moves 
+	the gun to the correct angle to fire on the target.**/
+	void doGun() {
+	    long time;
+	    long nextTime;
+	    Point2D.Double p;
+	    p = new Point2D.Double(target.x, target.y);
+	    for (int i = 0; i < 10; i++){
+	        nextTime = 
+	    (intMath.round((getRange(getX(),getY(),p.x,p.y)/(20-(3*firePower))));
+	        time = getTime() + nextTime;
+	        p = target.guessPosition(time);
+	    }
+	    /**Turn the gun to the correct angle**/
+	    double gunOffset = getGunHeadingRadians() - 
+	                  (Math.PI/2 - Math.atan2(p.y - getY(), p.x - getX()));
+	    setTurnGunLeftRadians(normaliseBearing(gunOffset));
+	}
+	double normaliseBearing(double ang) {
+	    if (ang > Math.PI)
+	        ang -= 2*PI;
+	    if (ang < -Math.PI)
+	        ang += 2*Math.PI;
+	    return ang;
+	}
+	public double getrange(double x1,double y1, double x2,double y2) {
+	    double x = x2-x1;
+	    double y = y2-y1;
+	    double h = Math.sqrt( x*x + y*y );
+	    return h;	
 	}
 }
