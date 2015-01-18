a83407c9570f829b5d993648c58fbe6859f782d0
Gavin <wingman639@yahoo.com.cn>
2015-01-18 20:40:50 +0800

    remove useless code

--- src/gavin/Observer.java
+++ src/gavin/Observer.java
@@ -72,36 +72,4 @@ public class Observer extends AdvancedRobot {
 		setTurnGunRightRadians(offset);
 	}
 	
-	/**This function predicts the time of the intersection between the 
-	bullet and the target based on a simple iteration.  It then moves 
-	the gun to the correct angle to fire on the target.**/
-	void doGun() {
-	    long time;
-	    long nextTime;
-	    Point2D.Double p;
-	    p = new Point2D.Double(target.x, target.y);
-	    for (int i = 0; i < 10; i++){
-	        nextTime = 
-	    (intMath.round((getRange(getX(),getY(),p.x,p.y)/(20-(3*firePower))));
-	        time = getTime() + nextTime;
-	        p = target.guessPosition(time);
-	    }
-	    /**Turn the gun to the correct angle**/
-	    double gunOffset = getGunHeadingRadians() - 
-	                  (Math.PI/2 - Math.atan2(p.y - getY(), p.x - getX()));
-	    setTurnGunLeftRadians(normaliseBearing(gunOffset));
-	}
-	double normaliseBearing(double ang) {
-	    if (ang > Math.PI)
-	        ang -= 2*PI;
-	    if (ang < -Math.PI)
-	        ang += 2*Math.PI;
-	    return ang;
-	}
-	public double getrange(double x1,double y1, double x2,double y2) {
-	    double x = x2-x1;
-	    double y = y2-y1;
-	    double h = Math.sqrt( x*x + y*y );
-	    return h;	
-	}
 }
